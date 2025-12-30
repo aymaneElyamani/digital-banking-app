377da6d73d7e7de7cd614dfd697b6babdf6b5b92
aymaneElyamani <aymaneelyamani30@gmail.com>
2025-12-30 17:40:20 +0000

    Add DTO classes for account operations and banking entities

---
+++ spring-backend/src/main/java/ma/mundia/springbackend/dtos/AccountOperationDTO.java
@@ -0,0 +1,16 @@
+package ma.mundia.springbackend.dtos;
+
+import lombok.Data;
+import ma.mundia.springbackend.enums.OperationType;
+
+import java.util.Date;
+
+@Data
+public class AccountOperationDTO {
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    private String description;
+}
+
---
+++ spring-backend/src/main/java/ma/mundia/springbackend/dtos/CurrentBankAccountDTO.java
@@ -0,0 +1,16 @@
+package ma.mundia.springbackend.dtos;
+
+import lombok.Data;
+import ma.mundia.springbackend.enums.AccountStatus;
+
+import java.util.Date;
+
+@Data
+public class CurrentBankAccountDTO extends BankAccountDTO {
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    private CustomerDTO customerDTO;
+    private double overDraft;
+}
